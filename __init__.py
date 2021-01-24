06623e2fe8e498a9bd390b6cb1cac2114c194fb2
Aleksander Starostka <aleksander.starostka@gmail.com>
2021-01-24 22:14:13 +0100

    Add files via upload
    BeautifulSoup4.4.0 wrapper that allows to find/iter sub tags using partial 
    key/value/key+value names to find matching HTML/XML tags in DOM from
    previously scraped page or directly downloaded from Web. Use it as a 
    wrapper that simplifies usage of native BeautifulSoup

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .ws4 import WebTag, WebSalad
+
+__all__ = [WebTag, WebSalad]
+__author__ = 'Aleksander Starostka #cx3# aleksander.starostka@gmail.com'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='websalad',
+    version='1.0',
+    packages=[''],
+    url='http://starostka.prv.pl',
+    license='OpenSource',
+    author='cx3',
+    author_email='aleksander.starostka@gmail.com',
+    description='BeautifulSoup4 wrapper'
+)
